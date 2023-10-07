20a068fea2563cabc2605354332e428dcbae3660
Anna Nowak <annov723@gmail.com>
2023-10-08 00:24:24 +0200

    second try

--- calculatorek/src/calculatorek/Main.java
+++ calculatorek/src/calculatorek/Main.java
@@ -3,7 +3,7 @@ package calculatorek;
 public class Main {
 	public static void main(String[] args) {
 		
-		Calculatorek calc = new Calculatorek();
+		Calculatorek calc = new Calculatorek(); //to start the program
 
 	}
 }
